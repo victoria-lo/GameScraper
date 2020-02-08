5356f1a834d371a313bca3f937378fd0f85a0e96
VicLo <victoria2666@gmail.com>
2020-02-07 23:54:55 -0500

    update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+var fs = require('fs');
 
 async function scrapeGames(url){
     const browser = await puppeteer.launch();
@@ -6,8 +7,27 @@ async function scrapeGames(url){
     await page.goto(url); //this page go to url
     const titles = await page.evaluate(() => Array.from(document.querySelectorAll('div.info > h3'), element => element.textContent));
     const prices = await page.evaluate(() => Array.from(document.querySelectorAll('div.info > p.b3'), element => element.textContent));
-    console.log(titles, prices);
+    const dates = await page.evaluate(() => Array.from(document.querySelectorAll('div.info > p.row-date'), element => element.textContent));
+    let json = [];
+    for(let i = 0; i< titles.length; i++){
+        var title = titles[i];
+        var price = prices[i];
+        var date = dates[i];
+        json.push([title,price, date]);
+    }
+    writeToFile(JSON.stringify(json,null,2));
+
     browser.close();
 }
 
 scrapeGames('https://www.nintendo.com/games/nintendo-switch-new-releases/');
+
+function writeToFile(json){
+    fs.writeFile("data.json", json, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+
